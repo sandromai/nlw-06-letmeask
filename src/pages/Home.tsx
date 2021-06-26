f00dc274566e097a1a06aeac870b932da2904c35
Sandro Mai <sandromai6@gmail.com>
2021-06-26 15:20:26 -0300

    Complete Day 4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -42,6 +42,12 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Room already closed.');
+
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   }
 
